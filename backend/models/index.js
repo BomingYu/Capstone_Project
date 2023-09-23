80108237fa1cca263c2969c9ad826cc329b1afe5
Boom_Y <bomingy@gmail.com>
2023-09-23 23:43:59 +1200

    product model created

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,13 @@
 "use strict"
 
 const User = require('./user')
+const Product = require("./product")
 
 async function init() {
     await User.sync()
+    await Product.sync()
 }
 
 init();
 
-module.exports = {User}
+module.exports = {User , Product}
---
+++ backend/models/product.js
@@ -0,0 +1,65 @@
+const { DataTypes , Model } = require("sequelize");
+const connectdb = require("../dbConnect");
+const { dataTool } = require("echarts");
+const sequelizeInstance = connectdb.Sequelize;
+
+class Product extends Model {}
+
+Product.init({
+    id:{
+        type : DataTypes.INTEGER,
+        allowNull : false,
+        autoIncrement : true,
+        primaryKey : true
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull : false,
+        require : true
+    },
+    barcode:{
+        type : DataTypes.STRING,
+        allowNull : true
+    },
+    category:{
+        type : DataTypes.STRING,
+        required : true,
+        allowNull : false
+    },
+    price:{
+        type : DataTypes.DECIMAL(10,2),
+        allowNull : false,
+        required : true,
+        validate : {isDecimal : true}
+    },
+    unit:{
+        type : DataTypes.STRING,
+        allowNull : true,
+        required : true
+    },
+    stock:{
+        type : DataTypes.DECIMAL(10,2),
+        allowNull : false,
+        defaultValue : 0
+    },
+    description:{
+        type : DataTypes.TEXT,
+        allowNull : true
+    },
+    picUrl : {
+        type : DataTypes.STRING,
+        allowNull : true
+    },
+    available : {
+        type : DataTypes.BOOLEAN,
+        allowNull : false,
+        defaultValue : true
+    }
+},{
+    sequelize : sequelizeInstance,
+    timestamps : true,
+    freezeTableName : true,
+    modelName : "products"
+})
+
+module.exports = Product;
